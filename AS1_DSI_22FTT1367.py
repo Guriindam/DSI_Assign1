6f783bbd64eb9a066fec8985876d2448a12f10d9
Adib“ <mafiaboss9696@gmail.com>
2024-02-11 11:29:31 +0800

    trying to implement side bar and adding the first visualization  (bar chart highlight)

--- AS1_DSI_22FTT1367.py
+++ AS1_DSI_22FTT1367.py
@@ -1,1 +1,34 @@
-print("Assignment please do")
+
+import pandas as pd
+import numpy as np 
+import streamlit as st
+import altair as alt
+
+
+df = pd.read_csv("Salary.csv")
+st.title("Salary across different demographics")
+
+with st.sidebar.form("MONEY"):
+    st.sidebar.header("Select Filter of Choosing:")
+    selected_x_var = st.sidebar.selectbox("x variable", df.columns)
+    selected_y_var = st.sidebar.selectbox("y variable",df.columns)
+    st.form_submit_button("submit")
+
+#Bar Chart of Job Titles
+
+jobtitle_count = df["Job Title"].value_counts().reset_index()
+jobtitle_count.columns = ['Job Title', 'Count']
+popular_job = jobtitle_count.loc[jobtitle_count["Count"].idxmax(), 'Job Title']
+
+fig = alt.Chart(jobtitle_count).mark_bar().encode(
+    x='Job Title:O',
+    y="Count:Q",
+    # The highlight will be set on the result of a conditional statement
+    color=alt.condition(
+        alt.datum["Job Title"] == popular_job,
+        alt.value('orange'),    
+        alt.value('steelblue')   
+    )
+).properties(width=600)
+
+st.altair_chart(fig)
