7abb6ffa4517190a0251101208325d9191f9eeb6
Adib“ <mafiaboss9696@gmail.com>
2024-02-13 09:32:43 +0800

    added pie chart and fixed submit button

--- AS1_DSI_22FTT1367.py
+++ AS1_DSI_22FTT1367.py
@@ -3,32 +3,64 @@ import pandas as pd
 import numpy as np 
 import streamlit as st
 import altair as alt
-
+import seaborn as sns
+import matplotlib.pyplot as plt
+from bokeh.plotting import figure
 
 df = pd.read_csv("Salary.csv")
 st.title("Salary across different demographics")
 
-with st.sidebar.form("MONEY"):
-    st.sidebar.header("Select Filter of Choosing:")
-    selected_x_var = st.sidebar.selectbox("x variable", df.columns)
-    selected_y_var = st.sidebar.selectbox("y variable",df.columns)
-    st.form_submit_button("submit")
+with st.sidebar:
+    with st.form('money'):
+        st.header("Select Filter of Choosing:")
+        selected_x_var = st.selectbox("x variable", df.columns)
+        selected_y_var = st.selectbox("y variable", df["Job Title"].unique())
+        st.form_submit_button("submit") 
 
-#Bar Chart of Job Titles
+    filtered_job = df[df["Job Title"] == selected_y_var]
+        
 
-jobtitle_count = df["Job Title"].value_counts().reset_index()
-jobtitle_count.columns = ['Job Title', 'Count']
-popular_job = jobtitle_count.loc[jobtitle_count["Count"].idxmax(), 'Job Title']
+with st.container():
+        
+    jobtitle_count = df["Job Title"].value_counts().reset_index()
+    jobtitle_count.columns = ['Job Title', 'Count']
+    popular_job = jobtitle_count.loc[jobtitle_count["Count"].idxmax(), 'Job Title']
 
-fig = alt.Chart(jobtitle_count).mark_bar().encode(
-    x='Job Title:O',
-    y="Count:Q",
-    # The highlight will be set on the result of a conditional statement
-    color=alt.condition(
-        alt.datum["Job Title"] == popular_job,
-        alt.value('orange'),    
-        alt.value('steelblue')   
+    fig = alt.Chart(jobtitle_count).mark_bar().encode(
+        x='Job Title:O',
+        y="Count:Q",
+        # The highlight will be set on the result of a conditional statement
+        color=alt.condition(
+            alt.datum["Job Title"] == popular_job,
+            alt.value('orange'),    
+            alt.value('steelblue')   
+        )
     )
-).properties(width=600)
 
-st.altair_chart(fig)
+    st.altair_chart(fig, use_container_width= True)
+
+
+with st.container():
+
+    chart = alt.Chart(filtered_job).mark_point().encode(
+        x=alt.X('Age', title='Age'),
+        y=alt.Y(selected_x_var, title=selected_x_var),
+    ).properties(width=500)
+
+    st.altair_chart(chart)  
+    #Gender Male Female Pie Chart
+
+
+with st.container():
+
+    gender_count = df["Gender"].value_counts().reset_index()
+    base = alt.Chart(gender_count).encode(
+        alt.Theta("Gender:Q").stack(True),
+        alt.Color("Gender:N").legend(None)
+    )
+
+    pie = base.mark_arc(outerRadius=120)
+    text = base.mark_text(radius=140, size=20).encode(text="Gender:N")
+
+    pie + text
+
