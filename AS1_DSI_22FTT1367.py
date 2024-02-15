862e14aae0dff3e856e671684f4e57d73cd96557
Adib“ <mafiaboss9696@gmail.com>
2024-02-15 23:56:18 +0800

    testing level of education

--- AS1_DSI_22FTT1367.py
+++ AS1_DSI_22FTT1367.py
@@ -6,9 +6,12 @@ import altair as alt
 import seaborn as sns
 import matplotlib.pyplot as plt
 from bokeh.plotting import figure
+import plotly.express as px
+
+
 
 df = pd.read_csv("Salary.csv")
-st.title("Salary across different demographics")
+st.title("Salary Across Different Demographics")
 
 with st.sidebar:
     with st.form('money'):
@@ -16,12 +19,16 @@ with st.sidebar:
         selected_x_var = st.selectbox("x variable", df.columns)
         selected_y_var = st.selectbox("y variable", df["Job Title"].unique())
         st.form_submit_button("submit") 
-
+        #st.header("Select Filter of Choosing:")
     filtered_job = df[df["Job Title"] == selected_y_var]
         
 
+#All Job Title Bar Chart
+
 with st.container():
-        
+    
+    st.subheader('Job Titles')
+    st.write("This Bar Chart shows the different Job titles that can be found in the data set, the highlighted bar shows the most job in the data set")
     jobtitle_count = df["Job Title"].value_counts().reset_index()
     jobtitle_count.columns = ['Job Title', 'Count']
     popular_job = jobtitle_count.loc[jobtitle_count["Count"].idxmax(), 'Job Title']
@@ -32,35 +39,85 @@ with st.container():
         # The highlight will be set on the result of a conditional statement
         color=alt.condition(
             alt.datum["Job Title"] == popular_job,
-            alt.value('orange'),    
+            alt.value('red'),    
             alt.value('steelblue')   
         )
     )
 
     st.altair_chart(fig, use_container_width= True)
 
+#Age filter Scatter Plot
 
 with st.container():
 
+    st.subheader('Age scatter plot (With Filter)')
+    st.write("This scatter plot shows the different ages accroding to the filters")
     chart = alt.Chart(filtered_job).mark_point().encode(
         x=alt.X('Age', title='Age'),
         y=alt.Y(selected_x_var, title=selected_x_var),
     ).properties(width=500)
 
-    st.altair_chart(chart)  
-    #Gender Male Female Pie Chart
+    st.altair_chart(chart)
+
+      
+#Gender Male Female Pie Chart
+
+with st.container():
+
+    st.subheader("Gender Pie Chart")
+    st.write("This Pie chart shows the percentage of Male and Female of the selected Job Title")
+    filtered_gender_count = filtered_job["Gender"].value_counts().reset_index()
+    #gender_count = df["Gender"].value_counts().reset_index()
+    fig = px.pie(filtered_gender_count , values='Gender', names= df["Gender"].unique(), title='Male Female Percentage')
+    st.plotly_chart(fig)
 
 
 with st.container():
 
-    gender_count = df["Gender"].value_counts().reset_index()
-    base = alt.Chart(gender_count).encode(
-        alt.Theta("Gender:Q").stack(True),
-        alt.Color("Gender:N").legend(None)
+    average_salary = df.groupby('Job Title')['Salary'].mean().reset_index()
+    average_salary = average_salary.sort_values(by = 'Salary',ascending = False)
+    top_10 = average_salary.head(10)
+    avg_chart = alt.Chart(top_10).mark_bar().encode(
+        x=alt.X('Job Title:O', title='Job Title',sort='-y'),
+        y=alt.Y('Salary', title='Job Average Salary'),
+        color=alt.value('blue')
     )
+    st.altair_chart(avg_chart, use_container_width= True)
 
-    pie = base.mark_arc(outerRadius=120)
-    text = base.mark_text(radius=140, size=20).encode(text="Gender:N")
 
-    pie + text
+#Average salary for selected
+#Fix the first visualization                                                                                                                                            
+
+# do visualization not based on different column, do based on the inside
+
+
+
+with st.container():
+
+    st.subheader('Salary scatter plot (Gender Filter)')
+    st.write("This scatter plot shows the different ages accroding to the filters")
+    chart = alt.Chart(filtered_job).mark_point().encode(
+        x=alt.X('Age', title='Age'),
+        y=alt.Y(selected_x_var, title=selected_x_var),
+    ).properties(width=500)
+
+    st.altair_chart(chart)
+
+    #with st.form('education filter'):
+        #st.write("Filter by Education Level")
+        #selected_level = st.selectbox('Education Level', df['Education Level'].unique)
+        #st.form_submit_button("submit") 
+
+    #filtered_level = filtered_job[filtered_job['Education'] == selected_level]
+
+
+    #with st.container():
+        #st.subheader('Salary and Years Of Experience')
+        #fig3 = alt.Chart(filtered_job).mark_circle().encode(
+            #x=alt.X('Education', title='Education Level'),
+            #y=alt.Y('Salary', title='Salary'),
+            #color='Gender'
+        #).properties(width=500)
+        
+    #st.altair_chart(fig3)
 
